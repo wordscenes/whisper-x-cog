02e2714bd6ad0e4507a87bc70a214e7fdb1ecd81
Kai Chang <kaijchang@gmail.com>
2025-10-03 11:06:05 -0700

    load align_models for target languages

--- predict.py
+++ predict.py
@@ -14,6 +14,22 @@ temperature_increment_on_fallback = 0.2
 # Not v3! See "rejected experiments" in readme
 WHISPER_MODEL = 'large-v2'
 
+TARGET_LANGUAGES = [
+    'ar',
+    'zh',
+    'en',
+    'fr',
+    'de',
+    'hi',
+    'id',
+    'it',
+    'ja',
+    'ko',
+    'pt',
+    'ru',
+    'es',
+    'tr',
+]
 
 def report_versions():
     print(f'Using whisperx version {version("whisperx")}')
@@ -31,9 +47,16 @@ class Predictor(BasePredictor):
         self.model = whisperx.load_model(
             WHISPER_MODEL,
             device='cuda',
-            compute_type="float16",
-            download_root=str(MODEL_CACHE)
+            compute_type='float16',
+            download_root=str(MODEL_CACHE),
         )
+        self.align_models = {}
+        for lang in TARGET_LANGUAGES:
+            self.align_models[lang] = whisperx.load_align_model(
+                language_code=lang,
+                device='cuda',
+                download_root=str(MODEL_CACHE),
+            )
 
     def predict(self) -> str:  # pyright: ignore[reportIncompatibleMethodOverride]
         return "Hello, world!"
